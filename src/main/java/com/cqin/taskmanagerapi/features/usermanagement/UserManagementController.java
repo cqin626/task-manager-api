d0f18c93daaad4dc984dac82acd7166a188d6f4a
cqin626 <qinhoh@gmail.com>
2026-01-15 14:03:50 +0800

    Remove addUser endpoint from UserManagementController

--- src/main/java/com/cqin/taskmanagerapi/features/usermanagement/UserManagementController.java
+++ src/main/java/com/cqin/taskmanagerapi/features/usermanagement/UserManagementController.java
@@ -7,15 +7,10 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cqin.taskmanagerapi.common.responses.APIResponse;
-import com.cqin.taskmanagerapi.features.usermanagement.dtos.CreateUserRequest;
 import com.cqin.taskmanagerapi.features.usermanagement.dtos.GetUserResponse;
-
-import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -31,12 +26,5 @@ public class UserManagementController {
       List<GetUserResponse> users = userManagementService.getUsers();
 
       return ResponseEntity.status(HttpStatus.OK).body(APIResponse.success(users));
-   }
-
-   @PostMapping()
-   public ResponseEntity<APIResponse<GetUserResponse>> addUser(@RequestBody @Valid CreateUserRequest createUserReq) {
-      GetUserResponse createUserRes = userManagementService.addUser(createUserReq);
-
-      return ResponseEntity.status(HttpStatus.CREATED).body(APIResponse.success(createUserRes));
    }
 }
