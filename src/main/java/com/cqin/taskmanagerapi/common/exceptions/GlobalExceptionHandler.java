ff8244b5178008df9508ac464c92c91314fee136
cqin626 <qinhoh@gmail.com>
2026-01-15 14:06:03 +0800

    Fix the issue where invalid HTTP method to an endpoint is treated as internal server error

--- src/main/java/com/cqin/taskmanagerapi/common/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/cqin/taskmanagerapi/common/exceptions/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.cqin.taskmanagerapi.common.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -13,45 +14,52 @@ import com.cqin.taskmanagerapi.common.responses.APIResponse;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-   @ExceptionHandler(NoHandlerFoundException.class)
-   public ResponseEntity<APIResponse<Void>> handleInvalidEndpoint(NoHandlerFoundException ex) {
-      return ResponseEntity
-            .status(HttpStatus.NOT_FOUND)
-            .body(APIResponse.error(ex.getMessage()));
-   }
+      @ExceptionHandler(NoHandlerFoundException.class)
+      public ResponseEntity<APIResponse<Void>> handleInvalidEndpoint(NoHandlerFoundException ex) {
+            return ResponseEntity
+                        .status(HttpStatus.NOT_FOUND)
+                        .body(APIResponse.error(ex.getMessage()));
+      }
 
-   @ExceptionHandler(HttpMessageNotReadableException.class)
-   public ResponseEntity<APIResponse<Void>> handleInvalidJson(HttpMessageNotReadableException ex) {
-      return ResponseEntity
-            .status(HttpStatus.BAD_REQUEST)
-            .body(APIResponse.error("Malformed JSON request"));
-   }
+      @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+      public ResponseEntity<APIResponse<Void>> handleMethodNotAllowed(HttpRequestMethodNotSupportedException ex) {
+            return ResponseEntity
+                        .status(HttpStatus.METHOD_NOT_ALLOWED)
+                        .body(APIResponse.error("Request method " + ex.getMethod() + " is not supported"));
+      }
 
-   @ExceptionHandler(HttpException.class)
-   public ResponseEntity<APIResponse<Void>> handleHttpException(HttpException ex) {
-      return ResponseEntity
-            .status(ex.getStatus())
-            .body(APIResponse.error(ex.getMessage()));
-   }
+      @ExceptionHandler(HttpMessageNotReadableException.class)
+      public ResponseEntity<APIResponse<Void>> handleInvalidJson(HttpMessageNotReadableException ex) {
+            return ResponseEntity
+                        .status(HttpStatus.BAD_REQUEST)
+                        .body(APIResponse.error("Malformed JSON request"));
+      }
 
-   @ExceptionHandler(MethodArgumentNotValidException.class)
-   public ResponseEntity<APIResponse<Void>> handleInvalidRequestBody(MethodArgumentNotValidException ex) {
-      String[] message = ex.getBindingResult()
-            .getFieldErrors()
-            .stream()
-            .map(err -> err.getField() + " " + err.getDefaultMessage())
-            .toArray(String[]::new);
+      @ExceptionHandler(HttpException.class)
+      public ResponseEntity<APIResponse<Void>> handleHttpException(HttpException ex) {
+            return ResponseEntity
+                        .status(ex.getStatus())
+                        .body(APIResponse.error(ex.getMessage()));
+      }
 
-      return ResponseEntity
-            .status(HttpStatus.BAD_REQUEST)
-            .body(APIResponse.error(message));
-   }
+      @ExceptionHandler(MethodArgumentNotValidException.class)
+      public ResponseEntity<APIResponse<Void>> handleInvalidRequestBody(MethodArgumentNotValidException ex) {
+            String[] message = ex.getBindingResult()
+                        .getFieldErrors()
+                        .stream()
+                        .map(err -> err.getField() + " " + err.getDefaultMessage())
+                        .toArray(String[]::new);
 
-   @ExceptionHandler(Exception.class)
-   public ResponseEntity<APIResponse<Void>> handleInternalServerError(Exception ex) {
-      // TODO: Add logging
-      return ResponseEntity
-            .status(HttpStatus.INTERNAL_SERVER_ERROR)
-            .body(APIResponse.error("Internal server error"));
-   }
+            return ResponseEntity
+                        .status(HttpStatus.BAD_REQUEST)
+                        .body(APIResponse.error(message));
+      }
+
+      @ExceptionHandler(Exception.class)
+      public ResponseEntity<APIResponse<Void>> handleInternalServerError(Exception ex) {
+            // TODO: Add logging
+            return ResponseEntity
+                        .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                        .body(APIResponse.error("Internal server error"));
+      }
 }
